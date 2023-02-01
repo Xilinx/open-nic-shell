f99f525531b7d097bb9ea61b9f211383c38b7ea1
ninaleechie <richard.baum@jacobs.com>
2023-02-01 21:59:36 +0000

    Adding support for Alveo U55C

---
+++ constr/au55c/timing.xdc
@@ -0,0 +1,38 @@
+# *************************************************************************
+#
+# Copyright 2020 Xilinx, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# *************************************************************************
+create_clock -period 10.000 -name pcie_refclk [get_ports pcie_refclk_p]
+
+set_false_path -through [get_ports pcie_rstn]
+
+set axis_aclk [get_clocks -of_object [get_nets axis_aclk]]
+foreach cmac_clk [get_clocks -of_object [get_nets cmac_clk*]] {
+    set_max_delay -datapath_only -from $axis_aclk -to $cmac_clk 4.000
+    set_max_delay -datapath_only -from $cmac_clk -to $axis_aclk 3.103
+}
+
+create_pblock pblock_packet_adapter_tx
+add_cells_to_pblock [get_pblocks pblock_packet_adapter_tx] [get_cells -quiet {cmac_port*.packet_adapter_inst/tx_inst}]
+resize_pblock [get_pblocks pblock_packet_adapter_tx] -add {CLOCKREGION_X0Y4:CLOCKREGION_X1Y5}
+
+create_pblock pblock_packet_adapter_rx
+add_cells_to_pblock [get_pblocks pblock_packet_adapter_rx] [get_cells -quiet {cmac_port*.packet_adapter_inst/rx_inst}]
+resize_pblock [get_pblocks pblock_packet_adapter_rx] -add {CLOCKREGION_X2Y4:CLOCKREGION_X3Y5}
+
+create_pblock pblock_qdma_subsystem
+add_cells_to_pblock [get_pblocks pblock_qdma_subsystem] [get_cells -quiet [list qdma_subsystem_inst]]
+resize_pblock [get_pblocks pblock_qdma_subsystem] -add {SLR0}
---
+++ src/cmac_subsystem/vivado_ip/cmac_usplus_0_au55c.tcl
@@ -0,0 +1,49 @@
+# *************************************************************************
+#
+# Copyright 2020 Xilinx, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# *************************************************************************
+set cmac_usplus cmac_usplus_0
+create_ip -name cmac_usplus -vendor xilinx.com -library ip -module_name $cmac_usplus -dir ${ip_build_dir}
+set_property -dict { 
+    CONFIG.CMAC_CAUI4_MODE {1}
+    CONFIG.NUM_LANES {4x25}
+    CONFIG.GT_REF_CLK_FREQ {161.1328125}
+    CONFIG.USER_INTERFACE {AXIS}
+    CONFIG.GT_DRP_CLK {125.00}
+    CONFIG.ENABLE_AXI_INTERFACE {1}
+    CONFIG.INCLUDE_STATISTICS_COUNTERS {1}
+    CONFIG.CMAC_CORE_SELECT {CMACE4_X0Y3}
+    CONFIG.GT_GROUP_SELECT {X0Y24~X0Y27}
+    CONFIG.LANE1_GT_LOC {X0Y24}
+    CONFIG.LANE2_GT_LOC {X0Y25}
+    CONFIG.LANE3_GT_LOC {X0Y26}
+    CONFIG.LANE4_GT_LOC {X0Y27}
+    CONFIG.LANE5_GT_LOC {NA}
+    CONFIG.LANE6_GT_LOC {NA}
+    CONFIG.LANE7_GT_LOC {NA}
+    CONFIG.LANE8_GT_LOC {NA}
+    CONFIG.LANE9_GT_LOC {NA}
+    CONFIG.LANE10_GT_LOC {NA}
+    CONFIG.RX_GT_BUFFER {1}
+    CONFIG.GT_RX_BUFFER_BYPASS {0}
+    CONFIG.INS_LOSS_NYQ {20}
+    CONFIG.INCLUDE_RS_FEC {1}
+    CONFIG.ETHERNET_BOARD_INTERFACE {qsfp0_4x}
+    CONFIG.DIFFCLK_BOARD_INTERFACE {qsfp0_refclk0}
+    CONFIG.ENABLE_PIPELINE_REG {1}
+} [get_ips $cmac_usplus]
+set_property CONFIG.RX_MIN_PACKET_LEN $min_pkt_len [get_ips $cmac_usplus]
+set_property CONFIG.RX_MAX_PACKET_LEN $max_pkt_len [get_ips $cmac_usplus]
